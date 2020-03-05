b98df5d6632475c9bd0dbffbf1331b464271306f
decimal-X <>
2020-03-05 22:13:40 +0900

    Title

--- src/Scenes/SceneTitle.js
+++ src/Scenes/SceneTitle.js
@@ -1,8 +1,19 @@
+import Phaser from "phaser";
+
 class SceneTitle extends Phaser.Scene {
     
-    constructor() {
-        super({ key: 'SceneTitle', active: true });
+    constructor ()
+    {
+        super();
     }
-    create() {
+
+    create(){
+        var rect;
+        rect = new Phaser.Geom.Rectangle(50, 200, 300, 50);
+        this.add.graphics({ fillStyle: { color: 0x0000ff } }).fillRectShape(rect);
+        this.add.text(50, 200, 'Here We Go', {fontSize: 50});
+        
     }
 }
+
+export default SceneTitle;
