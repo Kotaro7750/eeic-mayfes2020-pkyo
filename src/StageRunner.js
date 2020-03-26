0a81f421dbc400aee8184b718f88f4a795bbbd79
Kotaro7750 <7750koutarou@gmail.com>
2020-03-26 16:47:15 +0900

    update

--- src/StageRunner.js
+++ src/StageRunner.js
@@ -12,7 +12,7 @@ class StageRunner {
     this.tileSets = import('./stage/' + this.stageDir + '/tilesets.png');
 
     const [tileMap, tileSets] = await Promise.all(
-        [this.tileMap, this.tileSets]
+      [this.tileMap, this.tileSets]
     );
     return [tileMap.default, tileSets.default];
   }
@@ -24,7 +24,7 @@ class StageRunner {
     this.blockFuncs = import('./stage/' + this.stageDir + '/Blocks.js');
 
     const [xmlFilePath, stageConfig, blockDefs, blockFuncs] = await Promise.all(
-        [this.xmlFilePath, this.stageConfig, this.blockDefs, this.blockFuncs]
+      [this.xmlFilePath, this.stageConfig, this.blockDefs, this.blockFuncs]
     );
 
     return [xmlFilePath.default, stageConfig.default, blockDefs.blocks, blockFuncs];
